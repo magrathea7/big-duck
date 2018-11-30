f6e19a93b4e20335db8a4ea06f27cb2b7ecc80e3
Ralf Kehl <test-ente@posteo.de>
2018-11-30 19:07:04 +0100

    modified b01

---
+++ calc.js
@@ -0,0 +1,29 @@
+
+
+const add = (x, y) => {
+    return x + y;
+}
+
+const subtract = (x, y) => {
+    return x - y;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+}
+
+const divide = (x, y) => {
+    return x / y;
+}
+
+const square = (x) => {
+    return x * x;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    square,
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const calc = require('./calc');
+
+let x = 7, y = 8, z =0;
+
+z = calc.add(x, y);
+console.log('berechnet: ', z);
